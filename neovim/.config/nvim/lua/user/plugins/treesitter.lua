cd68892f272d1caad736377235e422c1dddda790
Jason Noonan <jason.noonan@pdq.com>
2023-07-20 20:55:45 -0500

    feat: treesitter all

--- neovim/.config/nvim/lua/user/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/user/plugins/treesitter.lua
@@ -1,23 +1,6 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "typescript",
-      "javascript",
-      "jsdoc",
-      "elixir",
-      "graphql",
-      "rust",
-      "tsx",
-      "toml",
-      "fish",
-      "json",
-      "yaml",
-      "css",
-      "html",
-      "lua",
-      "regex",
-      "haskell",
-    },
-  },
+	"nvim-treesitter/nvim-treesitter",
+	opts = {
+		ensure_installed = "all",
+	},
 }
