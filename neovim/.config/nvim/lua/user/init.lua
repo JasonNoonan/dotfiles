ee2af986c981ca8795aa7c95c9773b3a95cf42ef
Jason Noonan <jason.noonan@pdq.com>
2023-09-13 08:14:53 -0500

    refactor: dap configuration

--- neovim/.config/nvim/lua/user/init.lua
+++ neovim/.config/nvim/lua/user/init.lua
@@ -38,27 +38,6 @@ return {
 			},
 		},
 	},
-	dap = {
-		adapters = {
-			coreclr = {
-				type = "executable",
-				command = "/usr/local/bin/netcoredbg/netcoredbg",
-				args = { "--interpreter=vscode" },
-			},
-		},
-		configurations = {
-			cs = {
-				{
-					type = "coreclr",
-					name = "launch - netcoredbg",
-					request = "launch",
-					program = function()
-						return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/net6.0", "file")
-					end,
-				},
-			},
-		},
-	},
 	lsp = {
 		servers = {
 			"lexical",
--- neovim/.config/nvim/lua/user/plugins/mason.lua
+++ neovim/.config/nvim/lua/user/plugins/mason.lua
@@ -1,41 +1,93 @@
 return {
-  { "williamboman/mason.nvim", opts = { PATH = "append" } },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      automatic_installation = true,
-      ensure_installed = {
-        "tsserver",
-        "elixirls",
-        "rust_analyzer",
-        "lua_ls",
-        "yamlls",
-        "html",
-      },
-    },
-  },
-  {
-    "jay-babu/mason-null-ls.nvim",
-    config = function()
-      local mason_null_ls = require "mason-null-ls"
-      local null_ls = require "null-ls"
-      mason_null_ls.setup {
-        ensure_installed = { "prettierd", "eslint_d", "stylua", "eslint-lsp", "rustfmt" },
-        handlers = {
-          prettierd = function()
-            null_ls.register(
-              null_ls.builtins.formatting.prettierd.with { extra_filetypes = { "markdown", "rmd", "qmd" } }
-            )
-          end,
-        },
-      }
-    end,
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    opts = {
-      ensure_installed = { "elixir" },
-      automatic_setup = true,
-    },
-  },
+	{ "williamboman/mason.nvim", opts = { PATH = "append" } },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			automatic_installation = true,
+			ensure_installed = {
+				"tsserver",
+				"rust_analyzer",
+				"lua_ls",
+				"yamlls",
+				"html",
+			},
+		},
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		config = function()
+			local mason_null_ls = require("mason-null-ls")
+			local null_ls = require("null-ls")
+			mason_null_ls.setup({
+				ensure_installed = { "prettierd", "eslint_d", "stylua", "eslint-lsp", "rustfmt" },
+				handlers = {
+					prettierd = function()
+						null_ls.register(null_ls.builtins.formatting.prettierd.with({
+							extra_filetypes = { "markdown", "rmd", "qmd" },
+						}))
+					end,
+				},
+			})
+		end,
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		opts = {
+			automatic_setup = true,
+			handlers = {
+				coreclr = function(_)
+					local dap = require("dap")
+					dap.adapters.coreclr = {
+						type = "executable",
+						command = "/usr/local/bin/netcoredbg",
+						args = { "--interpreter=vscode" },
+					}
+
+					dap.configurations.cs = {
+						{
+							type = "coreclr",
+							name = "launch - netcoredbg",
+							request = "launch",
+							preLaunchTask = "build",
+							program = "~/workspace/janus/src/Janus.Web/bin/Debug/net6.0/Janus.Web.dll",
+							cwd = "~/workspace/janus/src/Janus.Web",
+							stopAtEntry = false,
+							serverReadyAction = {
+								action = "openExternally",
+								pattern = "^\\s*Now listening on:\\s+(https?://\\S+)",
+							},
+							env = {
+								ASPNETCORE_ENVIRONMENT = "Development",
+							},
+						},
+					}
+				end,
+				elixir = function(_)
+					local dap = require("dap")
+
+					dap.adapters.mix_task = {
+						type = "executable",
+						command = "/Users/jasonnoonan/.local/share/nvim/mason/packages/elixir-ls/debugger.sh", -- debugger.bat for windows
+						args = {},
+					}
+
+					dap.configurations.elixir = {
+						{
+							type = "mix_task",
+							name = "mix test",
+							task = "test",
+							taskArgs = { "--trace" },
+							request = "launch",
+							startApps = true, -- for Phoenix projects
+							projectDir = "${workspaceFolder}",
+							requireFiles = {
+								"test/**/test_helper.exs",
+								"test/**/*_test.exs",
+							},
+						},
+					}
+				end,
+			},
+		},
+	},
 }
